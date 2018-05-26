ed5d22c3aadc0fefc4e3148f736dd069504cc196
Leonardo Everson <llinharespinheiro@gmail.com>
2018-05-26 17:48:51 -0300

    gráficos

---
+++ app/src/main/java/com/leonardo/mqtt_client/Main2Activity.java
@@ -0,0 +1,53 @@
+package com.leonardo.mqtt_client;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+public class Main2Activity extends AppCompatActivity {
+
+    MqttClient mqttHelper;
+    TextView dataReceived;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    private void startMqtt() {
+        //Cria instância da classe
+        mqttHelper = new MqttClient(getApplicationContext());
+
+
+        //Configura métodos de callback
+        mqttHelper.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean b, String s) {
+
+            }
+
+            @Override
+            public void connectionLost(Throwable throwable) {
+
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
+                Log.w("Debug", mqttMessage.toString());
+                dataReceived.setText(mqttMessage.toString());
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+
+            }
+        });
+    }
+ }
+
--- app/src/main/java/com/leonardo/mqtt_client/MainActivity.java
+++ app/src/main/java/com/leonardo/mqtt_client/MainActivity.java
@@ -1,8 +1,11 @@
 package com.leonardo.mqtt_client;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
@@ -11,41 +14,32 @@ import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 public class MainActivity extends AppCompatActivity {
 
-    MqttClient mqttHelper;
+    Button bt1, bt2;
 
-    TextView dataReceived;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    private void startMqtt() {
-        //Cria instância da classe
-        mqttHelper = new MqttClient(getApplicationContext());
+        bt1 = (Button) findViewById(R.id.bt1);
+        bt2 = (Button) findViewById(R.id.bt2);
 
-        //Configura métodos de callback
-        mqttHelper.setCallback(new MqttCallbackExtended() {
+
+        //Activity com dados dos sensores
+        bt1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void connectComplete(boolean b, String s) {
-
+            public void onClick(View v) {
+                Intent it = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(it);
             }
+        });
 
+        bt2.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void connectionLost(Throwable throwable) {
-
-            }
-
-            @Override
-            public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
-                Log.w("Debug", mqttMessage.toString());
-                dataReceived.setText(mqttMessage.toString());
-            }
-
-            @Override
-            public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+            public void onClick(View v) {
 
             }
         });
     }
+    
 }
